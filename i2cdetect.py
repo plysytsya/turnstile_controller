fcb37eececdddd96a8cd043e0049e0f9a1dcc567
Pavlo <pavlo.lysytsya@gmail.com>
2024-03-19 18:34:48 +0100

    wip

---
+++ i2cdetect.py
@@ -0,0 +1,15 @@
+import smbus2
+import logging
+
+
+def i2cdetect(bus_number):
+    bus = smbus2.SMBus(bus_number)
+    devices = []
+    for device in range(128):
+        try:
+            bus.read_byte(device)
+            devices.append(hex(device))
+        except Exception as e:
+            logging.exception(e)
+            logging.error("NO LCD-DISPLAY FOUND")
+    return devices
