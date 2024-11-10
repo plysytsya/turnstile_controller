f181d8dddb0b0be9afc962336fddef76717aae92
plysytsy <pavlo@zorrobytes.es>
2024-11-10 15:14:24 +0100

    wip

--- qr.py
+++ qr.py
@@ -432,7 +432,7 @@ async def main_loop():
         if shared_list:
             qr_data = shared_list.pop(0)
             logger.info(f"Received QR data: {qr_data}")
-            display_on_lcd("Verificando", "QR...")
+            #display_on_lcd("Verificando", "QR...")
             verify_customer(qr_data["customer-uuid"], qr_data["timestamp"])
         await asyncio.sleep(0.2)  # 1-second delay to avoid busy-waiting
 
