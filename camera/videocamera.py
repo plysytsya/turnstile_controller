26669838bfd45caea94b5d1aebfc924f4a39a943
plysytsy <pavlo@zorrobytes.es>
2024-11-20 21:19:57 +0100

    set fps to 11

--- camera/videocamera.py
+++ camera/videocamera.py
@@ -30,7 +30,7 @@ class VideoCamera:
     FRAME_WIDTH = 480  # Width of the video frames
     FRAME_HEIGHT = 360  # Height of the video frames
 
-    DEFAULT_FPS = 20  # Default FPS for recording
+    DEFAULT_FPS = 11  # Default FPS for recording
 
     # Video recording parameters
     VIDEO_CODEC = 'XVID'  # Codec used for recording video
