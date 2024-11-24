aea72df3b37afed4cc98bc81c36b977e61f54cf1
plysytsy <pavlo@zorrobytes.es>
2024-11-24 16:54:36 +0100

    wip

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def login(hostname, username, password, logger):
     payload = {"email": username, "password": password}
     headers = {"Content-Type": "application/json"}
     logger.error(f"payload: {payload}")
-    response = requests.post(url, headers, payload)
+    response = requests.post(url, headers, json=payload)
 
     if response is None or response.status_code != 200:
         log_unsuccessful_request(response, logger)
