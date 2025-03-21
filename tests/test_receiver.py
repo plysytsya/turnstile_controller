e7bb1ac78dad4f447e5b8ce4bf29ceaf119c490a
Pavlo <pavlo.lysytsya@gmail.com>
2025-03-21 18:19:39 +0100

    Add basic RF24 sender and receiver test scripts.

--- tests/test_receiver.py
+++ tests/test_receiver.py
@@ -1,19 +1,17 @@
-from pyrf24 import RF24, RF24_PA_LOW
+from pyrf24 import RF24, RF24_PA_LOW, RF24_1MBPS
 import time
 
 # CE = GPIO26, CSN = SPI0_CE0
 radio = RF24(26, 0)
 address = b"1Node"
 
-
 def setup():
     radio.begin()
-    radio.set_pa_level(RF24_PA_LOW)
-    radio.set_data_rate(radio.BR_1MBPS)
-    radio.open_reading_pipe(1, address)
-    radio.listen = True  # Equivalent to startListening()
+    radio.setPALevel(RF24_PA_LOW)
+    radio.setDataRate(RF24_1MBPS)
+    radio.openReadingPipe(1, address)
+    radio.startListening()
     print("Receiver ready and listening...")
-
 
 def listen():
     while True:
@@ -22,7 +20,6 @@ def listen():
             length = radio.read(buffer)
             print("Received:", buffer[:length].decode('utf-8'))
         time.sleep(0.5)
-
 
 if __name__ == '__main__':
     setup()
