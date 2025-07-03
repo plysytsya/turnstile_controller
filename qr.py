950d8750b6d110e5f16057e7650853d3b6a3d7fb
Pavlo <pavlo.lysytsya@gmail.com>
2025-07-03 22:15:58 +0200

    wip

--- qr.py
+++ qr.py
@@ -46,10 +46,9 @@ USE_RS232 = int(os.getenv("USE_RS232", 0))
 if DIRECTION == "A":
     os.environ["ENTRANCE_UUID"] = os.getenv("ENTRANCE_UUID_A")
     if USE_LCD:
-        try:
-            os.environ["LCD_I2C_ADDRESS"] = detect_i2c_device_not_27(1)
-        except IndexError:
-            os.environ["LCD_I2C_ADDRESS"] = "0x27"  # Default address if detection fails
+        i2c_address_a = detect_i2c_device_not_27(1)
+        if i2c_address_a:
+            os.environ["LCD_I2C_ADDRESS"] = i2c_address_a
     os.environ["RELAY_PIN_DOOR"] = os.getenv("RELAY_PIN_A", "24")
     os.environ["RELAY_PIN_DISPLAY"] = os.getenv("RELAY_PIN_DISPLAY_A", "21")
     os.environ["IS_SERIAL_DEVICE"] = "True"
