4614dd47b9382c72b30a50bc98535f6a33e2cb16
Pavlo <pavlo.lysytsya@gmail.com>
2025-07-03 21:39:33 +0200

    wip

--- qr.py
+++ qr.py
@@ -47,11 +47,11 @@ if DIRECTION == "A":
     os.environ["ENTRANCE_UUID"] = os.getenv("ENTRANCE_UUID_A")
     if USE_LCD:
         if os.getenv("LCD_I2C_ADDRESS_A"):
-            i2c_address_a = os.getenv("LCD_I2C_ADDRESS_A")
+            os.environ["LCD_I2C_ADDRESS"] = os.getenv("LCD_I2C_ADDRESS_A")
         else:
             i2c_address_a = detect_i2c_device_not_27(1)
-        if i2c_address_a:
-            os.environ["LCD_I2C_ADDRESS"] = i2c_address_a
+            if i2c_address_a:
+                os.environ["LCD_I2C_ADDRESS"] = i2c_address_a
     os.environ["RELAY_PIN_DOOR"] = os.getenv("RELAY_PIN_A", "24")
     os.environ["RELAY_PIN_DISPLAY"] = os.getenv("RELAY_PIN_DISPLAY_A", "21")
     os.environ["IS_SERIAL_DEVICE"] = "True"
